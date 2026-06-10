542770b30742f4ddef11b4f4c1dde46aa0b05796
levovix <xlevovix@gmail.com>
2026-06-10 15:12:07 +0300

    add: fillHatching

--- src/rice.nim
+++ src/rice.nim
@@ -1,14 +1,14 @@
+import pkg/[chroma, opengl]
+export chroma, opengl
+
 import rice/[
   transform,
   gl, contexts, contextutils, antialiasing,
-  texts, primitives, polygonal3d, paths,
+  texts, primitives, polygonal3d, paths, hatching,
   fileformats
 ]
 export
   transform,
   gl, contexts, contextutils, antialiasing,
-  texts, primitives, polygonal3d, paths,
+  texts, primitives, polygonal3d, paths, hatching,
   fileformats
-
-import pkg/[chroma, opengl]
-export chroma, opengl
---
+++ src/rice/hatching.nim
@@ -0,0 +1,28 @@
+import pkg/[vmath, chroma, shady]
+import ./[contexts, gl]
+
+
+proc fillHatching*(
+  ctx: DrawContext,
+  mesh: Mesh,
+  color1, color2: Color,
+  dir: Vec3,
+  l1, l2: float32,
+  transform: Mat4 = mat4()
+) =
+  let shader = ctx.makeShader:
+    proc vert =
+      var pos {.inp.}: Vec3
+      var uv {.out.}: Vec3
+      gl_Position = @(transform) * vec4(pos.x, pos.y, pos.z, 1)
+      uv = pos
+
+    proc frag =
+      var glCol {.outGl.}: Vec4
+      if uv.dot(@(dir.normalize.Vec3)) mod (@(l1) + @(l2)) > @(l1):
+        glCol = @(color1.vec4)
+      else:
+        glCol = @(color2.vec4)
+
+  useAndPassUniforms shader
+  draw mesh
